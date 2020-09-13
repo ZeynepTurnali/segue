eb148ca1f1d8f601c10ea847131c87eefaf32e39
zeynepturnali <zeynepturnali23@gmail.com>
2020-09-13 21:47:54 +0300

    finished

--- Segue/ViewController.swift
+++ Segue/ViewController.swift
@@ -9,12 +9,25 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    var userName = ""
+    @IBOutlet var nameText: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func nextButton(_ sender: Any) {
+        userName = nameText.text!
+        performSegue(withIdentifier: "toSecondVC", sender: nil)
+    }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        if segue.identifier == "toSecondVC"{
+        let destinationVC = segue.destination as! SecondViewController
+            destinationVC.myName = userName
+        }
+        
+    }
 }
 
