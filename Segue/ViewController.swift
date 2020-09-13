daef55a6bb4d4fe18406f4ab48a6b432e96700a7
zeynepturnali <zeynepturnali23@gmail.com>
2020-09-13 17:32:59 +0300

    Initial Commit

---
+++ Segue/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Segue
+//
+//  Created by testinium on 13.09.2020.
+//  Copyright © 2020 testinium. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
